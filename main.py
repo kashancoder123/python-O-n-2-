e8e2af346748d80dcd205d8541a190fd7b981556
kashancoder123 <kashanhacker593@gmail.com>
2025-04-14 19:04:04 +0500

    python O(n^2)

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import random
 
-words = {"apple", "banana", "grape", "orange", "melon"}
+words = ["apple", "banana", "grape", "orange", "melon"]  # changed to list
 word = random.choice(words)
 scrambled = ''.join(random.sample(word, len(word)))
 
 print(" 🔤 Welcome to Word Scramble! ")
-print(f"Can you guess the word? SCramled word: {scrambled}")
+print(f"Can you guess the word? Scrambled word: {scrambled}")
 
-for attempt in range(3): # 3 tries ➡ O(n)
-    guess = input(f"Attempt {attempt + 1}:").lower()
+for attempt in range(3):  # 3 tries
+    guess = input(f"Attempt {attempt + 1}: ").lower()
     if guess == word:
         print(" 🎉 Correct! You guessed the word.")
         break
     else:
         print(" ❌ Try again.")
 else:
-    print(f"😢 Out of tries! The word was '{word}'.") 
+    print(f"😢 Out of tries! The word was '{word}'.")
